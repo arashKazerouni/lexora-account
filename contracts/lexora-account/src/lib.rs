b4c751d0c5d7013e2f24c942f7235f96472599c0
Arash Kazerouni <94610980+arashKazerouni@users.noreply.github.com>
2026-09-17 15:27:10 +0330

    fix: implement current CustomAccountInterface ABI

--- contracts/lexora-account/src/lib.rs
+++ contracts/lexora-account/src/lib.rs
@@ -1,7 +1,10 @@
 #![no_std]
 
 use soroban_sdk::{
-    contract, contractimpl, BytesN, Env, Vec,
+    auth::{Context, CustomAccountInterface},
+    contract, contractimpl,
+    crypto::Hash,
+    BytesN, Env, Vec,
 };
 
 #[contract]
@@ -16,28 +19,39 @@ impl LexoraAccount {
     }
 
     pub fn owner(env: Env) -> BytesN<32> {
-        env.storage()
-            .instance()
-            .get(&OWNER)
-            .unwrap()
+        env.storage().instance().get(&OWNER).unwrap()
     }
+}
+
+#[contractimpl]
+impl CustomAccountInterface for LexoraAccount {
+    type Error = Error;
+    type Signature = BytesN<64>;
 
     #[allow(non_snake_case)]
-    pub fn __check_auth(
+    fn __check_auth(
         env: Env,
-        signature_payload: BytesN<32>,
+        signature_payload: Hash<32>,
         signature: BytesN<64>,
-        _auth_context: Vec<soroban_sdk::auth::Context>,
-    ) {
-        let public_key: BytesN<32> = env
-            .storage()
-            .instance()
-            .get(&OWNER)
-            .unwrap();
+        _auth_context: Vec<Context>,
+    ) -> Result<(), Error> {
+        let public_key: BytesN<32> = env.storage().instance().get(&OWNER).unwrap();
 
-        env.crypto()
-            .ed25519_verify(&public_key, &signature_payload, &signature);
+        env.crypto().ed25519_verify(
+            &public_key,
+            &signature_payload.into(),
+            &signature,
+        );
+
+        Ok(())
     }
+}
+
+#[soroban_sdk::contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[repr(u32)]
+pub enum Error {
+    InvalidSignature = 1,
 }
 
 #[cfg(test)]
